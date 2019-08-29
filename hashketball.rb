737cfc75a47e5d6562d8793b42feb906bcc798bf
Shaqqour M <mshaqqourm@gmail.com>
2019-08-29 02:22:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,10 +9,10 @@ def game_hash
                    {:rebounds => 12}, {:assists => 12}, {:steals => 3}, 
                    {:blocks => 1}, {:slam_dunks => 1}
         },
-        "Alan Anderson" => {
-                   {:number => 0}, {:shoe => 16}, {:points => 22}, 
-                   {:rebounds => 12}, {:assists => 12}, {:steals => 3}, 
-                   {:blocks => 1}, {:slam_dunks => 1}
+        "Reggie Evans" => {
+                   {:number => 30}, {:shoe => 14}, {:points => 12}, 
+                   {:rebounds => 12}, {:assists => 12}, {:steals => 12}, 
+                   {:blocks => 12}, {:slam_dunks => 7}
         }
         ]
     },
