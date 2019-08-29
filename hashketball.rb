7f277f5e286cd62262321253f6663fb8282bb818
Shaqqour M <mshaqqourm@gmail.com>
2019-08-29 04:29:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -117,6 +117,7 @@ end
 
 def num_points_scored(name)
   game_hash.each do |team, details|
+    puts details[:players]
     if details[:players].include?(name)
       return details[:players][name][:points]
     end
