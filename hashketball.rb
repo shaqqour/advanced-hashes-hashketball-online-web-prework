5bdd64b22414e6242d783e81084665db05343f5a
Shaqqour M <mshaqqourm@gmail.com>
2019-08-29 04:27:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -117,8 +117,8 @@ end
 
 def num_points_scored(name)
   game_hash.each do |team, details|
-    if details.values[-1][0].keys.include?(name)
-      return details.values[-1][0][name][:points]
+    if details[:players].keys.include?(name)
+      return details[:players][name][:points]
     end
   end
   return "Invalid Player"
