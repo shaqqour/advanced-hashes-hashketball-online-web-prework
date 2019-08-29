1a5e34ebe5112780b2f1b3555a48fd07fd38e3a1
Shaqqour M <mshaqqourm@gmail.com>
2019-08-29 02:20:19 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,9 +3,18 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets"
       :color => ["Black", "White"]
-      :players => [{:number => 0}, {:shoe => }, {:points => }, 
-                   {:rebounds => }, {:assists => }, {:steals => }, 
-                   {:blocks => }, {:slam_dunks => }]
+      :players => [
+        "Alan Anderson" => {
+                   {:number => 0}, {:shoe => 16}, {:points => 22}, 
+                   {:rebounds => 12}, {:assists => 12}, {:steals => 3}, 
+                   {:blocks => 1}, {:slam_dunks => 1}
+        },
+        "Alan Anderson" => {
+                   {:number => 0}, {:shoe => 16}, {:points => 22}, 
+                   {:rebounds => 12}, {:assists => 12}, {:steals => 3}, 
+                   {:blocks => 1}, {:slam_dunks => 1}
+        }
+        ]
     },
     :away => {
       
