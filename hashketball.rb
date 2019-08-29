6dfbb9aa0617c516e7a0da59cea616e85ffa62bd
Shaqqour M <mshaqqourm@gmail.com>
2019-08-29 01:58:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 def game_hash
   game = {
     :home => {
-      :team_name => ""
+      :team_name => "Brooklyn Nets"
       :color => ["", ""]
       :players => [{:number => }, {:shoe => }, {:points => }, 
                    {:rebounds => }, {:assists => }, {:steals => }, 
